2e8dd1a0ac52c60e8bd6ba6f447504c4d48cc25c
Lucas Perrotta Barbosa <perrotta.lucas@gmail.com>
2022-02-21 12:13:11 -0300

    First Commit changing profile image, and some infos.

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'K4D - Lucas Perrotta Barbosa'
+export const siteTitle = 'K4DTech Learning NextJS'
 
 export default function Layout({ children, home }) {
   return (
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="K4D Junior Engineer Software learning how to build a personal website using Next.js"
         />
         <meta
           property="og:image"
